920c4d71a2dfb5302fd8a9894bd5acb1bda28b33
Dalzon Charles-Hébert <charlby5@gmail.com>
2020-11-19 12:18:01 -0600

    Test google oauth

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+require('dotenv').config();
 
 const app = express();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLECLIENTID,
+      clientSecret: process.env.GOOGLESECRETCLIENT,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 

233c7c18b1129755be7fe1d591b1e2f9aa2cc61c
Dalzon Charles-Hébert <charlby5@gmail.com>
2020-11-20 20:58:20 -0600

    Install and configure mongoose

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 require('./services/passport');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+
+// Connect to DB
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+  })
+  .then(() => {
+    console.log('Database connected');
+  })
+  .catch((err) => console.log(err));
 
 // routes
 app.use('/auth', authRoutes);

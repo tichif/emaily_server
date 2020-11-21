b7d554ccc156db01fc588d69e1f6fac109186313
Dalzon Charles-Hébert <charlby5@gmail.com>
2020-11-20 21:48:05 -0600

    Check if the user is already in the database

--- services/passport.js
+++ services/passport.js
@@ -12,7 +12,18 @@ passport.use(
       callbackURL: '/auth/google/callback',
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save();
+      User.findOne({ googleId: profile.id })
+        .then((existingUser) => {
+          if (existingUser) {
+            done(null, existingUser);
+          } else {
+            new User({ googleId: profile.id })
+              .save()
+              .then((user) => done(null, user))
+              .catch((err) => console.log(err));
+          }
+        })
+        .catch((err) => console.log(err));
     }
   )
 );

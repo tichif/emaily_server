e953f615f1a8275e8a1ea14139e4b9e3a423fd39
Dalzon Charles-Hébert <charlby5@gmail.com>
2020-11-24 08:12:03 -0600

    Refractor with Async/Await

--- services/passport.js
+++ services/passport.js
@@ -32,19 +32,14 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true,
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id })
-        .then((existingUser) => {
-          if (existingUser) {
-            done(null, existingUser);
-          } else {
-            new User({ googleId: profile.id })
-              .save()
-              .then((user) => done(null, user))
-              .catch((err) => console.log(err));
-          }
-        })
-        .catch((err) => console.log(err));
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        done(null, existingUser);
+      } else {
+        const user = await new User({ googleId: profile.id }).save();
+        done(null, user);
+      }
     }
   )
 );

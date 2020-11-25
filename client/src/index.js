6c47c7192b905f0419a795b5ba1a65777937a37a
Dalzon Charles-Hébert <charlby5@gmail.com>
2020-11-24 23:01:07 -0600

    Set up the react app and install some packages

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import App from './components/App';
+
+ReactDom.render(<App />, document.getElementById('root'));

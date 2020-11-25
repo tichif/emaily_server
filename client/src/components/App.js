38709fb000a76b210f5a0ee073a0bcb38d115ac9
Dalzon Charles-Hébert <charlby5@gmail.com>
2020-11-25 00:19:26 -0600

    - Set up react router - Create Header component - Install Materialize css

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Header from './Header';
 
 const App = () => {
-  return <div>Hi There</div>;
+  return (
+    <div>
+      <Router>
+        <Header />
+      </Router>
+    </div>
+  );
 };
 
 export default App;

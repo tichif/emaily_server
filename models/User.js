67fc300a3df66300f97f73127102bddac0a5b571
Dalzon Charles-Hébert <charlby5@gmail.com>
2020-11-28 20:16:04 -0600

    Add credits to the user and check if the user is authenticated

--- models/User.js
+++ models/User.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
   googleId: String,
+  credits: {
+    type: Number,
+    default: 0,
+  },
 });
 
 mongoose.model('User', UserSchema);
--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -4,15 +4,21 @@ const stripe = require('stripe')(
     : process.env.SECRET_STRIPE_KEY
 );
 
+const requireLogin = require('../middlewares/requireLogin');
+
 module.exports = (app) => {
   // charge customer money
-  app.post('/api/stripe', async (req, res) => {
+  app.post('/api/stripe', requireLogin, async (req, res) => {
     const charge = await stripe.charges.create({
       amount: 500, // 5$
       currency: 'usd',
       description: '$5 for 5 credits',
       source: req.body.id,
     });
-    console.log(charge);
+
+    req.user.credits += 5;
+    const user = await req.user.save();
+
+    res.status(200).send(user);
   });
 };

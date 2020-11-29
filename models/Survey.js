446ad62c5ef13318771f0b1fca1f1c9077ab1684
Dalzon Charles-Hébert <charlby5@gmail.com>
2020-11-29 15:39:57 -0600

    Create RecipientSchema and add relation survey and user

--- models/Survey.js
+++ models/Survey.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
+const RecipientSchema = require('./Recipient');
+
 const SurveySchema = new mongoose.Schema({
   title: String,
   subject: String,
   body: String,
-  recipients: [String],
+  recipients: [RecipientSchema],
   yes: {
     type: Number,
     default: 0,
@@ -13,6 +15,12 @@ const SurveySchema = new mongoose.Schema({
     type: Number,
     default: 0,
   },
+  _user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  dateSent: Date,
+  lastResponded: Date,
 });
 
 mongoose.model('Survey', SurveySchema);

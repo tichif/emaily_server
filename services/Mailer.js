458f2388536cb47075e2a49da84d410136b0e429
Dalzon Charles-Hébert <charlby5@gmail.com>
2020-11-30 22:42:55 -0600

    Finish the Mailer class

--- services/Mailer.js
+++ services/Mailer.js
@@ -2,6 +2,45 @@ const sendGrid = require('sendgrid');
 
 const helpers = sendGrid.mail;
 
-class Mailer extends helpers.Mail {}
+class Mailer extends helpers.Mail {
+  constructor({ subject, recipients }, content) {
+    super();
+    this.from_email = new helpers.Email('charlby5@gmail.com');
+    this.subject = subject;
+    this.body = new helpers.Content('text/html', content);
+    this.recipients = this.formatAddresses(recipients);
+
+    this.addContent(this.body); //addContent is provided by Sendgrid
+
+    // Enable click tracking in the email
+    this.addClickTracking();
+
+    this.addRecipients();
+  }
+
+  formatAddresses(addresses) {
+    return addresses.map(({ email }) => {
+      return new helpers.Email(email);
+    });
+  }
+
+  addClickTracking() {
+    const trackingSettings = new helpers.TrackingSettings();
+    const clickTracking = new helpers.ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
+
+  addRecipients() {
+    const personalize = new helpers.Personalization();
+
+    this.recipients.forEach((recipient) => {
+      personalize.addTo(recipient);
+    });
+
+    this.addPersonalization(personalize);
+  }
+}
 
 module.exports = Mailer;

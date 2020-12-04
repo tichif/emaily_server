5180d8c675fe5cf80e2ae767e3a0808a0eba2adb
Dalzon Charles-Hébert <charlby5@gmail.com>
2020-12-03 22:05:22 -0600

    Style the add survey button

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Dashboard = () => {
-  return <h2>Dashboard</h2>;
+  return (
+  <div>
+    Dashboard
+    <div className="fixed-action-btn">
+      <Link to="/surveys/new" className="btn-floating btn-large red">
+        <i className="material-icons">add</i>  
+      </Link>
+    </div>
+  </div>);
 };
 
 export default Dashboard;
---
+++ client/src/components/NewSurvey.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class NewSurvey extends Component {
+  render() {
+    return <div>New Survey</div>;
+  }
+}
+
+export default NewSurvey;

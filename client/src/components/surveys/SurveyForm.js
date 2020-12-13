8fffe81434ccc61aecd91607545f9fcd4fc11d11
Dalzon Charles-Hébert <charlby5@gmail.com>
2020-12-13 17:14:17 -0600

    Create SurveyFormReview component

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -40,7 +40,7 @@ class SurveyForm extends Component {
     return (
       <div>
         <form
-          onSubmit={this.props.handleSubmit((values) => console.log(values))}
+          onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}
         >
          {this.renderFields()}
          <Link to="/surveys" className="red btn-flat white-text">Cancel</Link>
